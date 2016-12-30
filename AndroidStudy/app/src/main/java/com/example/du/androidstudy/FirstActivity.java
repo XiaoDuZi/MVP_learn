90a16cc41f2e14a92c7042caed0824e3e75a2084
DuXinwu <13262083380@163.com>
2016-12-30 22:32:52 +0800

    Intent学习

--- AndroidStudy/app/src/main/java/com/example/du/androidstudy/FirstActivity.java
+++ AndroidStudy/app/src/main/java/com/example/du/androidstudy/FirstActivity.java
@@ -1,26 +1,85 @@
 package com.example.du.androidstudy;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class FirstActivity extends AppCompatActivity {
 
     private Button mButton;
+    private Button mButton2;
+    private Button mButton3;
+    private Button mButton4;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-        mButton= (Button) findViewById(R.id.button);
+
+        initViews();
+
+
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent("om.example.du.androidstudy.My");
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent("com.example.du.androidstudy.ACTION_START");
                 startActivity(intent);
             }
         });
+        mButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent("com.example.du.androidstudy.ACTION_START");
+                intent.addCategory("com.example.du.androidstudy.MY");
+                startActivity(intent);
+
+            }
+        });
+        mButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        mButton= (Button) findViewById(R.id.button);
+        mButton2= (Button) findViewById(R.id.button2);
+        mButton3= (Button) findViewById(R.id.button3);
+        mButton4= (Button) findViewById(R.id.button4);
+        mTextView= (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 1:
+                if (resultCode==RESULT_OK){
+                    String result=data.getStringExtra("data");
+                    Log.e("result",result);
+                    mTextView.setText(result);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
--- AndroidStudy/app/src/main/java/com/example/du/androidstudy/ThirdActivity.java
+++ AndroidStudy/app/src/main/java/com/example/du/androidstudy/ThirdActivity.java
@@ -1,13 +1,38 @@
 package com.example.du.androidstudy;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private Button mButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        mButton= (Button) findViewById(R.id.button5);
+        mButton1= (Button) findViewById(R.id.button6);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
     }
 }
